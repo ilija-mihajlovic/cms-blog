6ea43ed2e7ecdc2fe2fa1ccde057696caa8291f6
Ilija Mihajlovic <iilija879a@gmail.com>
2022-09-18 23:08:56 +0200

    setup, design and structure start

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.scss'
+import React, { useEffect, useState } from 'react'
+import { Layout } from '../components'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
